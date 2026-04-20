3af6f33064004ce0615e41608c560da517cecc86
Tmin1021 <128274812+Tmin1021@users.noreply.github.com>
2026-04-20 10:50:32 +0700

    UI: Update win and lose text

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,9 +1,18 @@
+using System.Runtime.CompilerServices;
+using System.Threading;
 using TMPro;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class UIController : MonoBehaviour
 {
     [SerializeField] TMP_Text healthText;
+    [SerializeField] GameObject winText;
+    [SerializeField] GameObject loseText;
+    [SerializeField] float winTime = 10f;
+    private float _timer;
+    private bool _lose = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void OnEnable()
     {
@@ -16,13 +25,23 @@ public class UIController : MonoBehaviour
 
     void Start()
     {
-        
+        _timer = winTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _timer -= Time.deltaTime;
+        if(_timer == 0f)
+        {
+            Time.timeScale = 0f;
+            winText.SetActive(true);
+        }
+        if(_lose == true)
+        {
+            Time.timeScale = 0f;
+            loseText.SetActive(true);
+        }
     }
 
     void UpdateHealthText(float currentHealth)

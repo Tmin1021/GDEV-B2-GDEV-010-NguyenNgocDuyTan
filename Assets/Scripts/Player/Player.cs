31c66755442285703626200376b7b81bf9ad470a
Tmin1021 <128274812+Tmin1021@users.noreply.github.com>
2026-04-20 09:41:49 +0700

    Initial Setup

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+            RaycastHit2D raycastHit = Physics2D.Raycast(worldPoint, Vector2.zero, Mathf.Infinity);
+
+            if(raycastHit.collider != null)
+            {
+                // moving player by clicking mouse
+            }
+        }
+    }
+}
